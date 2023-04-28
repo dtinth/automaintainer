c3ef8d44b18fbc569267eb946a32887349a19fbb
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-28 20:01:55 +0700

    ‌

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,26 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-import { Env } from "@(-.-)/env";
-import { z } from "zod";
 
-const env = Env(
-  z.object({
-    CACHE_TRANSPORTER_TOKEN: z.string(),
-    CACHE_TRANSPORTER_URI: z.string().optional(),
-  })
-);
+const flag = (description: string) => ({
+  type: "boolean" as const,
+  description,
+});
 
 yargs(process.argv.slice(2))
   .demandCommand()
   .strict()
   .help()
   .command(
-    "push",
-    "push",
+    "$0",
+    "Runs the automaintainer",
     {
-      "cache-id": {
-        type: "string",
-        demand: true,
-      },
-      path: {
-        type: "string",
-        array: true,
-        demand: true,
-      },
+      package: flag(
+        "Updates package.json metadata field such as repository, bugs, homepage, etc."
+      ),
     },
     async (args) => {
       console.log(args);
-      if (!env.valid) {
-        console.error("Skipping push, environment is invalid.");
-        console.error(env.error);
-        process.exit(0);
-      }
-      console.log("push");
     }
   )
-  .command("pull", "pull", {}, async () => {
-    if (!env.valid) {
-      console.error("Skipping pull, environment is invalid.");
-      console.error(env.error);
-      process.exit(0);
-    }
-    console.log("pull");
-  })
-  .command("prune", "prune", {}, async () => {
-    const token = env.CACHE_TRANSPORTER_TOKEN;
-    console.log("prune");
-  })
-  .command("server", "server", {}, async () => {
-    const token = env.CACHE_TRANSPORTER_TOKEN;
-    console.log("server");
-  })
   .parse();
